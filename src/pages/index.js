84a90a292bad234f7830d5b52ff6613ef8d86ece
Mohamed Youssouf <mohamedyoussoufali@gmail.com>
2018-03-07 14:00:26 +0300

    Fixed issues with mobile screens and added google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,18 +36,15 @@ class IndexPage extends Component {
 
     this.clickhandler = this.clickhandler.bind(this)
   }
-
-  
-
   
   render() {
     return (
       <div>
+        <img src={Logo} alt="" id="logo"/>
         <ul className="choose-lang">
           <li><button type="button" onClick={() => this.clickhandler(ar, 'rtl')}>عربي</button></li>
           <li><button type="button" onClick={() => this.clickhandler(en, 'ltr')}>English</button></li>
         </ul>
-        <img src={Logo} alt="" id="logo"/>
         <h1 id="heading" dangerouslySetInnerHTML={{ __html: this.state.language.heading }}></h1>
         <p id="note">{this.state.language.note}</p>
         <a href="mailto:example@email.com" className="btn btn-default" id="cta-contact" onClick={()=>this.setState({btnText: 'mohamedyoussouf1994@gmail.com'})}>{this.state.btnText}</a>
@@ -74,24 +71,5 @@ class IndexPage extends Component {
     }.bind(this), 200);
   }
 }
-
-
-// const IndexPage = (props) => (
-  
-//     <div>
-//     <img src={Logo} alt="" id="logo"/>
-//     <h1 id="heading">محمد يوسف، <span>مصمم</span> و<span>مطور</span> واجهات مواقع مستقل</h1>
-//     <p id="note">
-//       هل لديك مشروع ممتع لي؟ :)
-//     </p>
-//     <a href="#" className="btn btn-default" id="cta-contact">تواصل معي</a>
-// {console.log(props)}
-//     <ul className="social-links">
-//       <li><a href="#" className="fa fa-twitter" title="قل مرحباً على twitter"></a></li>
-//       <li><a href="#" className="fa fa-linkedin" title="تواصل معي على linkedin"></a></li>
-//       <li><a href="#" className="fa fa-globe" title="اقرأ مقالاتي في مدونتي"></a></li>
-//     </ul>
-//   </div>
-// )
 
 export default IndexPage
