ef2ee2d727686ec9b2724fe7c6e0a7a955dfba6f
Mohamed Youssouf <mohamedyoussoufali@gmail.com>
2020-03-21 12:34:07 +0300

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,39 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      'display': [
+        '"Noto Kufi Arabic"',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      'body': [
+        '"Noto Naskh Arabic"',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
     extend: {
       colors: {
         purple: '#5C3AE8',
