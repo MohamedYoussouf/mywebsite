a63ca449527106c7ae6214fda4fcd54270fde1c4
Mohamed Youssouf <mohamedyoussoufali@gmail.com>
2020-03-28 18:05:55 +0300

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Link} from 'gatsby'
+import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Layout>
+    <div className="container-inner text-center py-20">
+      <h1>الصفحة المطلوبة غير موجودة!</h1>
+      <p className="text-lg mb-12">لم يتم العثور على الصفحة المطلوبة. قد يكون الرابط خاطئ أو الصفحة حذفت.</p>
+      <Link to="/" className="bg-purple px-5 py-2 text-white font-display font-bold rounded hover:no-underline focus:no-underline">عودة للصفحة الرئيسية</Link>
+    </div>
+  </Layout>
 )
 
 export default NotFoundPage
