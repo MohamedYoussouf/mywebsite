78183b646aaba7a1e1d7c7bec1b6537e3083017d
Mohamed Youssouf <mohamedyoussoufali@gmail.com>
2020-05-24 17:00:06 +0300

    Fix issues with responsive design

--- src/components/shareButtons.js
+++ src/components/shareButtons.js
@@ -4,10 +4,10 @@ const ShareButtons = (props) => {
     const url = 'http://www.example.com';
     return (
         <div className="text-center">
-            <h3 className="mb-8">أعجبك ما قرأت؟ شارك مع الأخرين</h3>
-            <div>
-            <a href={`https://www.facebook.com/sharer.php?u=${url}${props.url}`} className="inline-block w-48 py-2 bg-gray-900 text-white text-sm font-display font-bold rounded bg-facebook mx-3 hover:no-underline focus:no-underline"><i className="fab fa-facebook-f"/> شارك على فيسبوك</a>
-            <a href={`https://twitter.com/intent/tweet?url=${url}&text=${props.title}&via=medmya`} className="inline-block w-48 py-2 bg-gray-900 text-white text-sm font-display font-bold rounded bg-twitter mx-3 hover:no-underline focus:no-underline"><i className="fab fa-twitter"/> شارك على تويتر</a>
+            <h3 className="mb-8 text-lg md:text-xl">أعجبك ما قرأت؟ شارك مع الأخرين</h3>
+            <div className="flex flex-col justify-center md:flex-row md:space-x-2 md:space-x-reverse">
+                <a href={`https://www.facebook.com/sharer.php?u=${url}${props.url}`} className="w-full block mb-5 md:w-48 py-4 md:py-2 bg-gray-900 text-white text-sm font-display font-bold rounded bg-facebook hover:no-underline focus:no-underline"><i className="fab fa-facebook-f"/> شارك على فيسبوك</a>
+                <a href={`https://twitter.com/intent/tweet?url=${url}&text=${props.title}&via=medmya`} className="w-full block mb-5 md:w-48 py-4 md:py-2 bg-gray-900 text-white text-sm font-display font-bold rounded bg-twitter hover:no-underline focus:no-underline"><i className="fab fa-twitter"/> شارك على تويتر</a>
             </div>
         </div>
     )
