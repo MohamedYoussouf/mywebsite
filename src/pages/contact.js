3a57d61b707f878ec35350d981d5562495168203
Mohamed Youssouf <mohamedyoussoufali@gmail.com>
2020-05-25 14:02:17 +0300

    Make contact page responsive

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,18 +12,18 @@ const ContactPage = (props) => (
           <h1 className="text-3xl">تواصل معي</h1>
           <p>إذا كان عندك مشروع وتريد من يساعدك به، أو تريد فقط القاء التحية فيمكنك تواصل معي عبر النموذج التواصل التالي أو عبر البريد الإلكتروني</p>
         </div>
-        <div className="flex flex-row mt-8">
-          <div className="w-4/6 pl-5">
+        <div className="flex flex-col md:flex-row mt-8">
+          <div className="w-auto md:w-4/6 pl-5 mb-16">
             <form className="flex flex-col">
-              <div className="flex flex-row">
-                <div className="w-1/2 mx-3 mb-8">
+              <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/2 mx-3 mb-8">
                   <label className="block font-display font-bold text-black mb-5">الاسم الكامل</label>
                   <input 
                     className="py-3 px-5 bg-gray-200 border-gray-300 border-2 focus:shadow-outline rounded-md w-full" 
                     type="text" 
                     placeholder="أدخل اسمك الكامل"/>
                 </div>
-                <div className="w-1/2 mx-3 mb-8">
+                <div className="md:w-1/2 mx-3 mb-8">
                   <label className="block font-display font-bold text-black mb-5">البريد الإلكتروني</label>
                   <input 
                     className="py-3 px-5 bg-gray-200 border-gray-300 border-2 focus:shadow-outline rounded-md w-full" 
@@ -60,7 +60,7 @@ const ContactPage = (props) => (
               </div>
             </form>
           </div>
-          <div className="w-2/6">
+          <div className="w-auto md:w-2/6">
             <div className="mb-16">
               <label className="block font-display font-bold text-black mb-5">البريد الالكتروني</label>
               <a href="mailto:">demo@email.com</a>
